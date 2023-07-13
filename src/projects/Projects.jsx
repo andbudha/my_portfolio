64572ed31e4cf36f6e1a12295d543447d09833c8
andbudha <andreibartov@gmail.com>
2023-07-13 16:08:26 +0200

    Project comp. updated

--- src/projects/Projects.jsx
+++ src/projects/Projects.jsx
@@ -1,18 +1,24 @@
 import styles from './Projects.module.css'
 import { Project } from "./project/Project";
 import { Title } from '../components/common/title/Title';
+import todolistImg from './project_images/todolist.png'
 
 export const Projects = () => {
 
     const description = 'Brief project description: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, esse. Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+
+    const todolistImage = {
+        backgroundImage: `url(${todolistImg})`
+    }
 
     return (
         <div className={styles.projects}>
             <div className={styles.projects_container}>
                 <Title title={'My Projects'} />
                 <div className={styles.projects_box_container}>
-                    <Project description={description} />
-                    <Project description={description} />
+                    <Project description={description} projectTitle={'Todolist-JS'} image={todolistImage} />
+                    <Project description={description} projectTitle={'Counter'} image={todolistImage} />
+                    <Project description={description} projectTitle={'Todolist-ReactJS'} image={todolistImage} />
                 </div>
             </div>
         </div>
--- src/projects/project/Project.jsx
+++ src/projects/project/Project.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
+
 import styles from './Project.module.css'
 
+
 export const Project = (props) => {
+
     return (
         <div className={styles.project_box}>
-            <div className={styles.img_container}>
-                <a href="#">Click For More</a>
+            <div className={styles.img_container} style={props.image}>
+                <a href="">Click For More</a>
             </div>
-            <div className={styles.project_description}>
-                <h3>Project Name</h3>
-                <p>
+            <div className={styles.project_description_box}>
+                <h3 className={styles.project_title}>{props.projectTitle}</h3>
+                <p className={styles.project_description}>
                     {props.description}
                 </p>
             </div>
