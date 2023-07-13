3af7bd53bacb407d9da4341cfa3a378a38dd6d95
andbudha <andreibartov@gmail.com>
2023-07-13 12:34:24 +0200

    Main comp. updated/ media query breakpoint added

--- src/contact/Contact.jsx
+++ src/contact/Contact.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
 import styles from './Contact.module.css'
+import { Title } from '../components/common/title/Title';
+
 export const Contact = () => {
     return (
         <div className={styles.contact}>
             <div className={styles.contact_container}>
-                <h3>Fill In The Form To Hire Me:</h3>
+                <Title title={'Get In Touch'} />
                 <form className={styles.form_container}>
                     <input className={styles.input_field} type="text" />
                     <input className={styles.input_field} type="text" />
