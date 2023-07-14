b84db3eab796340de34ba74fa9c72912cbd64bce
andbudha <andreibartov@gmail.com>
2023-07-14 15:32:24 +0200

    Main comp. updated / buttons&img changing added

--- src/main/Main.jsx
+++ src/main/Main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './Main.module.css';
 import profileImgOne from './img/no_background_1.png'
-import profileImgTwo from './img/no_background_2.png'
+import profileImgTwo from './img/no_background_2.1.png'
 
 
 export const Main = () => {
@@ -20,12 +20,14 @@ export const Main = () => {
                     <h1>I am <span className={styles.name}>Andrei</span></h1>
                     <span className={styles.position_span}>A FRONT-END DEVELOPER</span>
                     <div className={styles.btn_container}>
-
+                        <a href="#" className={styles.projects_btn}>MY PROJECTS</a>
+                        <a href="#" className={styles.contact_btn}>GET IN TOUCH</a>
                     </div>
                 </div>
                 <div className={styles.main_img} style={profileImgMain}></div>
+                <div className={styles.secondary_img} style={profileImgSecondary}></div>
             </div>
 
-        </div>
+        </div >
     );
 };
