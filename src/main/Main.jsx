19c3ab92acc544010a11fca82deda9cc3eba111d
andbudha <andreibartov@gmail.com>
2023-07-14 13:30:19 +0200

    profile fotos added/Main comp. updated

--- src/main/Main.jsx
+++ src/main/Main.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import styles from './Main.module.css';
+import profileImgOne from './img/no_background_1.png'
+import profileImgTwo from './img/no_background_2.png'
 
 
 export const Main = () => {
+    const profileImgMain = {
+        backgroundImage: `url(${profileImgOne})`
+    }
+
+    const profileImgSecondary = {
+        backgroundImage: `url(${profileImgTwo})`
+    }
     return (
         <div className={styles.main}>
             <div className={styles.main_container}>
@@ -10,8 +19,11 @@ export const Main = () => {
                     <span className={styles.greeting_span}>Hi There</span>
                     <h1>I am <span className={styles.name}>Andrei</span></h1>
                     <span className={styles.position_span}>A FRONT-END DEVELOPER</span>
+                    <div className={styles}>
+
+                    </div>
                 </div>
-                <div className={styles.main_img}></div>
+                <div className={styles.main_img} style={profileImgMain}></div>
             </div>
 
         </div>
