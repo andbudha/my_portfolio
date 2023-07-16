8860fabac9380846f55e14e39a865ac73b38e0f6
andbudha <andreibartov@gmail.com>
2023-07-16 22:54:17 +0200

    Footer component updated/styles& icons added

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import styles from './Footer.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faGithub, faTelegram, faSquareFacebook } from '@fortawesome/free-brands-svg-icons'
+
+
+
+
 
 export const Footer = () => {
     return (
@@ -7,13 +13,13 @@ export const Footer = () => {
             <div className={styles.footer_container}>
                 <h3 className={styles.fullname}>Andrei Bartov</h3>
                 <div className={styles.link_container}>
-                    <a className={styles.link} href="#"></a>
-                    <a className={styles.link} href="#"></a>
-                    <a className={styles.link} href="#"></a>
-                    <a className={styles.link} href="#"></a>
+                    <a className={styles.link} href="#"><FontAwesomeIcon icon={faLinkedin} className={styles.icon} style={{ color: "#ff4500", }} /></a>
+                    <a className={styles.link} href="#"><FontAwesomeIcon icon={faGithub} className={styles.icon} style={{ color: "#ff4500", }} /></a>
+                    <a className={styles.link} href="#"><FontAwesomeIcon icon={faTelegram} className={styles.icon} style={{ color: "#ff4500", }} /></a>
+                    <a className={styles.link} href="#"><FontAwesomeIcon icon={faSquareFacebook} className={styles.icon} style={{ color: "#ff4500", }} /></a>
                 </div>
-                <h3 className={styles.copyright}>&copy; All Rights Reserved</h3>
+                <h3 className={styles.copyright}><span>&copy;</span> All Rights Reserved</h3>
             </div>
-        </div>
+        </div >
     );
 };
