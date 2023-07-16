c97629259d5df6e4d6983a8b5996034a6d74ad76
andbudha <andreibartov@gmail.com>
2023-07-16 21:03:36 +0200

    Contact component updated/styles added

--- src/contact/Contact.jsx
+++ src/contact/Contact.jsx
@@ -4,13 +4,13 @@ import { Title } from '../components/common/title/Title';
 export const Contact = () => {
     return (
         <div className={styles.contact}>
+            <Title title={'Get In Touch'} />
             <div className={styles.contact_container}>
-                <Title title={'Get In Touch'} />
                 <form className={styles.form_container}>
-                    <input className={styles.input_field} type="text" />
-                    <input className={styles.input_field} type="text" />
-                    <textarea className={styles.textarea_field} name="" id="" cols="auto" rows="auto"></textarea>
-                    <input className={styles.submit_btn} type="submit" value={'SEND'} />
+                    <input className={styles.input_field} type="text" required placeholder={'Name*'} />
+                    <input className={styles.input_field} type="text" required placeholder={'Email*'} />
+                    <textarea className={styles.textarea_field} name="" id="" cols="auto" rows="auto" placeholder={'Your Message*'} ></textarea>
+                    <input className={styles.submit_btn} type="submit" value={'SEND MESSAGE'} />
                 </form>
             </div>
         </div>
--- src/skills/Skills.jsx
+++ src/skills/Skills.jsx
@@ -4,8 +4,6 @@ import { ProgressBar } from '../progressbar/ProgressBar';
 
 export const Skills = () => {
 
-    const description = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, esse. Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
-
     return (
         <div className={styles.skills}>
             <div className={styles.skills_container}>
