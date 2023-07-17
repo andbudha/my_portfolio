2af931e680aac34b18cd75e926f23e776fa3d901
andbudha <andreibartov@gmail.com>
2023-07-17 14:55:38 +0200

    Footer comp-styles updated

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -8,10 +8,10 @@ export const Footer = () => {
             <div className={styles.footer_container}>
                 <h3 className={styles.fullname}>Andrei Bartov</h3>
                 <div className={styles.link_container}>
-                    <a className={styles.link} href="#"><FontAwesomeIcon icon={faLinkedin} className={styles.icon} style={{ color: "#ff4500", }} /></a>
-                    <a className={styles.link} href="#"><FontAwesomeIcon icon={faGithub} className={styles.icon} style={{ color: "#ff4500", }} /></a>
-                    <a className={styles.link} href="#"><FontAwesomeIcon icon={faTelegram} className={styles.icon} style={{ color: "#ff4500", }} /></a>
-                    <a className={styles.link} href="#"><FontAwesomeIcon icon={faSquareFacebook} className={styles.icon} style={{ color: "#ff4500", }} /></a>
+                    <a className={styles.link} href="#"><FontAwesomeIcon icon={faLinkedin} className={styles.icon} /></a>
+                    <a className={styles.link} href="#"><FontAwesomeIcon icon={faGithub} className={styles.icon} /></a>
+                    <a className={styles.link} href="#"><FontAwesomeIcon icon={faTelegram} className={styles.icon} /></a>
+                    <a className={styles.link} href="#"><FontAwesomeIcon icon={faSquareFacebook} className={styles.icon} /></a>
                 </div>
                 <h3 className={styles.copyright}><span>&copy;</span> All Rights Reserved</h3>
             </div>
