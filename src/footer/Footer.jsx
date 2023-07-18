7d64efd7d98cff2efd3892e40302a06e7b8619e8
andbudha <andreibartov@gmail.com>
2023-07-18 09:31:17 +0200

    Footer/Projects updated/Fading effects added

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,17 +1,27 @@
-import styles from './Footer.module.scss'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLinkedin, faGithub, faTelegram, faSquareFacebook } from '@fortawesome/free-brands-svg-icons'
+import styles from './Footer.module.scss';
+import Flip from 'react-reveal/Flip';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub, faTelegram, faSquareFacebook } from '@fortawesome/free-brands-svg-icons';
 
 export const Footer = () => {
     return (
         <div className={styles.footer}>
             <div className={styles.footer_container}>
                 <h3 className={styles.fullname}>Andrei Bartov</h3>
+
                 <div className={styles.link_container}>
-                    <a className={styles.link} href="#"><FontAwesomeIcon icon={faLinkedin} className={styles.icon} /></a>
-                    <a className={styles.link} href="#"><FontAwesomeIcon icon={faGithub} className={styles.icon} /></a>
-                    <a className={styles.link} href="#"><FontAwesomeIcon icon={faTelegram} className={styles.icon} /></a>
-                    <a className={styles.link} href="#"><FontAwesomeIcon icon={faSquareFacebook} className={styles.icon} /></a>
+                    <Flip>
+                        <a className={styles.link} href="#"><FontAwesomeIcon icon={faLinkedin} className={styles.icon} /></a>
+                    </Flip>
+                    <Flip>
+                        <a className={styles.link} href="#"><FontAwesomeIcon icon={faGithub} className={styles.icon} /></a>
+                    </Flip>
+                    <Flip>
+                        <a className={styles.link} href="#"><FontAwesomeIcon icon={faTelegram} className={styles.icon} /></a>
+                    </Flip>
+                    <Flip>
+                        <a className={styles.link} href="#"><FontAwesomeIcon icon={faSquareFacebook} className={styles.icon} /></a>
+                    </Flip>
                 </div>
                 <h3 className={styles.copyright}><span>&copy;</span> All Rights Reserved</h3>
             </div>
--- src/projects/project/Project.jsx
+++ src/projects/project/Project.jsx
@@ -4,18 +4,21 @@ import Fade from 'react-reveal/Fade';
 export const Project = (props) => {
 
     return (
-        <div className={styles.project_box}>
-            <a href="#" className={styles.uni_btn}><span>CLICK FOR MORE...</span></a>
-            <div className={styles.img_container} style={props.image}>
-            </div>
-            <div className={styles.project_description_box}>
-                <div className={styles.title}>
-                    <h3 className={styles.project_title}>{props.projectTitle}</h3>
+        <Fade bottom>
+            <div className={styles.project_box}>
+                <a href="#" className={styles.uni_btn}><span>CLICK FOR MORE...</span></a>
+                <div className={styles.img_container} style={props.image}>
                 </div>
-                <p className={styles.project_description}>
-                    {props.description}
-                </p>
-            </div>
-        </div >
+                <div className={styles.project_description_box}>
+                    <div className={styles.title}>
+                        <h3 className={styles.project_title}>{props.projectTitle}</h3>
+                    </div>
+                    <p className={styles.project_description}>
+                        {props.description}
+                    </p>
+                </div>
+
+            </div >
+        </Fade>
     );
 };
