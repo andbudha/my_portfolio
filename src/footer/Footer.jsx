49ca43b141ed370b7c020750b5968730eaa30979
andbudha <andreibartov@gmail.com>
2023-07-18 10:07:00 +0200

    react typing effect library installed/Main updated

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,5 +1,5 @@
 import styles from './Footer.module.scss';
-import Flip from 'react-reveal/Flip';
+import Fade from 'react-reveal/Fade';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faGithub, faTelegram, faSquareFacebook } from '@fortawesome/free-brands-svg-icons';
 
@@ -10,18 +10,18 @@ export const Footer = () => {
                 <h3 className={styles.fullname}>Andrei Bartov</h3>
 
                 <div className={styles.link_container}>
-                    <Flip>
+                    <Fade bottom>
                         <a className={styles.link} href="#"><FontAwesomeIcon icon={faLinkedin} className={styles.icon} /></a>
-                    </Flip>
-                    <Flip>
+                    </Fade>
+                    <Fade bottom>
                         <a className={styles.link} href="#"><FontAwesomeIcon icon={faGithub} className={styles.icon} /></a>
-                    </Flip>
-                    <Flip>
+                    </Fade>
+                    <Fade bottom>
                         <a className={styles.link} href="#"><FontAwesomeIcon icon={faTelegram} className={styles.icon} /></a>
-                    </Flip>
-                    <Flip>
+                    </Fade>
+                    <Fade bottom>
                         <a className={styles.link} href="#"><FontAwesomeIcon icon={faSquareFacebook} className={styles.icon} /></a>
-                    </Flip>
+                    </Fade>
                 </div>
                 <h3 className={styles.copyright}><span>&copy;</span> All Rights Reserved</h3>
             </div>
--- src/main/Main.jsx
+++ src/main/Main.jsx
@@ -1,5 +1,5 @@
 import styles from './Main.module.scss';
-import Fade from 'react-reveal/Fade';
+import { TypeAnimation } from 'react-type-animation';
 import profileImgOne from './img/no_background_1.1.png';
 import profileImgTwo from './img/no_background_1.2.png';
 
@@ -15,21 +15,27 @@ export const Main = () => {
     return (
         <div className={styles.main}>
             <div className={styles.main_container}>
-                <Fade left>
-                    <div className={styles.greeting}>
-                        <span className={styles.greeting_span}>Hi There</span>
-                        <h1>I'm <span className={styles.name}>Andrei</span></h1>
-                        <span className={styles.position_span}>FRONT-END DEVELOPER</span>
-                        <div className={styles.btn_container}>
-                            <a href="#" className={styles.projects_btn}>MY PROJECTS</a>
-                            <a href="#" className={styles.contact_btn}>GET IN TOUCH</a>
-                        </div>
+                <div className={styles.greeting}>
+                    <span className={styles.greeting_span}>Hi There</span>
+                    <h1>I'm <span className={styles.name}>Andrei</span></h1>
+                    <TypeAnimation
+                        sequence={[
+                            // Same substring at the start will only be typed out once, initially
+                            'FRONT-END DEVELOPER',
+                            2000
+                        ]}
+                        wrapper="span"
+                        speed={300}
+                        repeat={1}
+                        className={styles.position_span}
+                    />
+                    <div className={styles.btn_container}>
+                        <a href="#" className={styles.projects_btn}>MY PROJECTS</a>
+                        <a href="#" className={styles.contact_btn}>GET IN TOUCH</a>
                     </div>
-                </Fade>
-                <Fade right>
-                    <div className={styles.main_img} style={profileImgMain}></div>
-                    <div className={styles.secondary_img} style={profileImgSecondary}></div>
-                </Fade>
+                </div>
+                <div className={styles.main_img} style={profileImgMain}></div>
+                <div className={styles.secondary_img} style={profileImgSecondary}></div>
             </div>
         </div >
     );
