087a27790ccdc772461a54c2f3eeb139e682eed1
andbudha <andreibartov@gmail.com>
2023-07-19 14:11:29 +0200

    Nav. com.  and media queries updated

--- src/nav/Nav.jsx
+++ src/nav/Nav.jsx
@@ -1,13 +1,43 @@
+import { Link, animateScroll as scroll } from "react-scroll";
 import styles from './Nav.module.scss';
+
 
 export const Nav = () => {
     return (
         <div className={styles.nav}>
             <div className={styles.nav_container}>
-                <a href="#main">Main</a>
-                <a href="#skills">Skills</a>
-                <a href="#projects">Projects</a>
-                <a href="#contact">Contact</a>
+                <Link
+                    activeClass={styles.active}
+                    to='main'
+                    spy={true}
+                    smooth={true}
+                    offset={0}
+                    duration={500}
+                >Main</Link>
+                <Link
+                    activeClass={styles.active}
+                    to='skills'
+                    spy={true}
+                    smooth={true}
+                    offset={-100}
+                    duration={500}
+                >Skills</Link>
+                <Link
+                    activeClass={styles.active}
+                    to='projects'
+                    spy={true}
+                    smooth={true}
+                    offset={-100}
+                    duration={500}
+                >Projects</Link>
+                <Link
+                    activeClass={styles.active}
+                    to='contact'
+                    spy={true}
+                    smooth={true}
+                    offset={-100}
+                    duration={500}
+                >Contact</Link>
             </div>
         </div>
     );
