1341403fcb3ea6b11ed626c79b0a04d5af25fc9e
andbudha <andreibartov@gmail.com>
2023-07-20 14:45:00 +0200

    Nav comp updated

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,10 +1,11 @@
 import styles from './Header.module.scss'
 import { BurgerNav } from '../nav/burgernav/BurgerNav';
+import { Nav } from '../nav/Nav';
 
 export const Header = () => {
     return (
         <div className={`${styles.header}`}>
-            {/* <Nav /> */}
+            <Nav />
             <BurgerNav />
         </div>
     );
