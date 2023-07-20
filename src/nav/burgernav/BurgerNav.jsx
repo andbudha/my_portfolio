337b36c81c8e4c4493e758d0354a2b4a21a5df2e
andbudha <andreibartov@gmail.com>
2023-07-20 13:52:36 +0200

    BurgerNav updated

--- src/nav/burgernav/BurgerNav.jsx
+++ src/nav/burgernav/BurgerNav.jsx
@@ -1,16 +1,25 @@
 import { Link, animateScroll as scroll } from "react-scroll";
 import styles from './BurgerNav.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons'
+import { useState } from "react";
 
 
 
 
 export const BurgerNav = () => {
+
+    const [display, setDisplay] = useState(false);
+
+    const activateBurgerHandler = () => {
+        setDisplay(!display);
+    }
+
     return (
         <div className={styles.burger_nav}>
-            <div className={styles.burger_nav_container}>
+            <div className={display ? `${styles.show_menu}` : `${styles.burger_nav_container}`}>
                 <Link
+                    onClick={activateBurgerHandler}
                     activeClass={styles.active}
                     to='main'
                     spy={true}
@@ -19,31 +28,38 @@ export const BurgerNav = () => {
                     duration={500}
                 >Main</Link>
                 <Link
+                    onClick={activateBurgerHandler}
                     activeClass={styles.active}
                     to='skills'
                     spy={true}
                     smooth={true}
-                    offset={-94}
+                    offset={-50}
                     duration={500}
                 >Skills</Link>
                 <Link
+                    onClick={activateBurgerHandler}
                     activeClass={styles.active}
                     to='projects'
                     spy={true}
                     smooth={true}
-                    offset={-95}
+                    offset={-51}
                     duration={500}
                 >Projects</Link>
                 <Link
+                    onClick={activateBurgerHandler}
                     activeClass={styles.active}
                     to='contact'
                     spy={true}
                     smooth={true}
-                    offset={-96}
+                    offset={-60}
                     duration={500}
                 >Contact</Link>
             </div>
-            <div className={styles.burger_btn}><FontAwesomeIcon icon={faBars} className={styles.burger} /></div>
+            {display ?
+                <div onClick={activateBurgerHandler} className={styles.burger_btn}><FontAwesomeIcon icon={faXmark} className={styles.burger} /></div> :
+                <div onClick={activateBurgerHandler} className={styles.burger_btn}><FontAwesomeIcon icon={faBars} className={styles.burger} /></div>
+            }
+
         </div>
     );
 }
