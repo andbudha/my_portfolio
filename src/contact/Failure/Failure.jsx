1ca0efea2ee3a140ed1f72657ae87cacf7ab38b0
andbudha <andreibartov@gmail.com>
2023-07-23 12:28:16 +0200

    styles updated

--- src/contact/Failure/Failure.jsx
+++ src/contact/Failure/Failure.jsx
@@ -6,8 +6,8 @@ export const Failure = () => {
     return (
         <div className={styles.failure_container}>
             <span className={styles.failure_icon}><FontAwesomeIcon icon={faCircleExclamation} className={styles.failure_icon} /></span>
-            <span className={styles.message}>Unexpected error,</span>
-            <span className={styles.message}> please, try again!</span>
+            <span className={styles.message}>Unexpected error!</span>
+            <span className={styles.message}> Please, try again.</span>
         </div>
 
     );
